62daf0df96025bfbc5a31e3ca1b6b587a8df7c5c
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-04-13 23:50:52 +0530

    modifr

--- node/nodes.py
+++ node/nodes.py
@@ -15,7 +15,7 @@ def planner_node(state: AgentState):
 def page_node(state: AgentState):
     pages = generate_pages(state['prompt'])
     print("Page running")
-    return {"pages": pages}
+    return {"pages": pages1}
 
 
 def ui_node(state: AgentState):
