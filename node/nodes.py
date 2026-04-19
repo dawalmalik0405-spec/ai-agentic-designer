c25ca3e028a35e04ea6976584f19372dcbc74423
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-04-19 09:27:37 +0530

    changed

--- node/nodes.py
+++ node/nodes.py
@@ -1,36 +1,45 @@
 from ai_agentic_designer.agents.page_agent import generate_pages
-from ai_agentic_designer.agents.planner_agent import planner
-from ai_agentic_designer.agents.state import AgentState
 from ai_agentic_designer.agents.theme_agent import generate_theme
 from ai_agentic_designer.agents.ui_agent import generate_ui
-from ai_agentic_designer.mcp_server.tools.figma_tool import sync_site_spec_to_figma
+from ai_agentic_designer.agents.asset_agent import generate_assets
+from ai_agentic_designer.agents.planner_agent import planner
+from ai_agentic_designer.agents.state import AgentState
+from ai_agentic_designer.mcp_server.tools.figma_tool import create_ui_frames
+
 
 
 def planner_node(state: AgentState):
-    site_spec = planner(state["prompt"])
+    plan = planner(state['prompt'])
     print("Planner running")
-    return {"site_spec": site_spec}
-
+    return {"plan": plan}
 
 def page_node(state: AgentState):
-    site_spec = generate_pages(state["site_spec"])
+    pages = generate_pages(state['prompt'], state['plan'])
     print("Page running")
-    return {"site_spec": site_spec}
+    return {"pages": pages}
 
 
 def ui_node(state: AgentState):
-    site_spec = generate_ui(state["site_spec"])
-    print("UI running")
-    return {"site_spec": site_spec}
-
+    ui_layout = generate_ui(state["pages"]["pages"],state["plan"])
+    print("ui running")
+    return {"ui": ui_layout}
 
 def theme_node(state: AgentState):
-    site_spec = generate_theme(state["site_spec"])
-    print("Theme running")
-    return {"site_spec": site_spec}
+    theme = generate_theme(state["ui"], state['plan'])
+    print("theme running")
+    return {"theme": theme}
 
+def asset_node(state: AgentState):  
+    assets = generate_assets(state['theme'],state['plan'])
+    print("assets running")
+    return {"assets": assets}
 
 def figma_node(state: AgentState):
-    figma_result = sync_site_spec_to_figma(state["site_spec"])
-    print("Figma running")
-    return {"figma_result": figma_result}
+    figma = create_ui_frames.invoke({
+    "layout": state['ui']['layout']})
+    print(state["ui"]["layout"])
+    print("figma toold runnning ")
+    if "layout" not in state["ui"]:
+        return {"figma": {}}
+    return {"figma": figma}
+
