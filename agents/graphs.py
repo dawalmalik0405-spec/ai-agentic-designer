8add341c8d80d2f832d500568071a1a83a81c40f
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-04-21 23:12:08 +0530

    d

--- agents/graphs.py
+++ agents/graphs.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph
 
-from ai_agentic_designer.node.nodes import page_node, ui_node, theme_node, planner_node
+from ai_agentic_designer.node.nodes import page_node, ui_node, design_node, planner_node
 from ai_agentic_designer.agents.state import AgentState
 
 
@@ -13,14 +13,14 @@ def create_agent_graph():
   workflow.add_node("plan", planner_node)
   workflow.add_node("page", page_node)
   workflow.add_node("ui", ui_node)
-  workflow.add_node("theme", theme_node)
+  workflow.add_node("design", design_node)
 
 
   workflow.set_entry_point("plan")
 
   workflow.add_edge("plan", "page")
-  workflow.add_edge("page", "theme")
-  workflow.add_edge("theme", "ui")
+  workflow.add_edge("page", "design")
+  workflow.add_edge("design", "ui")
 
   workflow.set_finish_point("ui")
 
