f3dbaafbd2cf274d82c3410d36c35478bed0d5d3
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-04-25 16:06:44 +0530

    c

---
+++ frontend/src/Components/ChatPanel.tsx
@@ -0,0 +1,12 @@
+export default function ChatPanel(){
+  return (
+
+    <div className="w-[30%] h-screen border-r border-white/10 text-white">
+        chat interface
+    </div>
+
+  
+  )
+
+  
+}
